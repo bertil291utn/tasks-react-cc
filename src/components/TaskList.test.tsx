18b9be9f2a0b46628637b98cc87922c6d35a7a7c
Bertil Tandayamo <bertilt4@gmail.com>
2023-12-22 17:07:23 -0500

    Set a functional test list

--- src/components/TaskList.test.tsx
+++ src/components/TaskList.test.tsx
@@ -1,7 +1,35 @@
-import reducer, { addTasks, TaskInitialState } from '../redux/taskReducer'
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import TasksScreen from '../layouts/TasksScreen';
+import { addTasks } from '../redux/taskReducer';
 
-test('Devuelve un listado de las tareas guardadas en redux', () => {
-  const INITIAL_STATE = { id: 1, description: "Tarea 1" }
-  const resp = reducer(TaskInitialState, addTasks(INITIAL_STATE));
-  expect(resp.tasks).toEqual([INITIAL_STATE])
+
+describe('TasksScreen Component', () => {
+  test('renders tasks list from Redux store', () => {
+    store.dispatch(addTasks({ id: 1, description: 'Tarea 1' }))
+
+    render(
+      <Provider store={store}>
+        <Router>
+          <TasksScreen />
+        </Router>
+      </Provider>
+    );
+
+    expect(screen.getByText('Tarea 1')).toBeInTheDocument();
+  });
 })
+
+
+
+
+
+
+
+// test('Devuelve un listado de las tareas guardadas en redux', () => {
+//   const INITIAL_STATE = { id: 1, description: "Tarea 1" }
+//   const resp = reducer(TaskInitialState, addTasks(INITIAL_STATE));
+//   expect(resp.tasks).toEqual([INITIAL_STATE])
+// })
