26bd48ca1755fadddec7f67c9289a5fa4984f13c
Bertil Tandayamo <bertilt4@gmail.com>
2023-12-22 10:42:50 -0500

    Set home list and tasks screen

---
+++ src/layouts/ListadoScreen.tsx
@@ -0,0 +1,24 @@
+// src/screens/ListadoScreen.tsx
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Lists from '../components/Lists';
+import { isLoadingListSelector } from '../redux/selectors';
+import { fetchElements } from '../redux/listReducer';
+
+const ListadoScreen: React.FC = () => {
+  const dispatch = useDispatch();
+  const IsLoading = useSelector(isLoadingListSelector);
+
+  useEffect(() => {
+    dispatch(fetchElements() as any);
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Pantalla de Listado</h1>
+      {IsLoading === 'pending'  ? <div>Cargando...</div> : <Lists />}
+    </div>
+  );
+};
+
+export default ListadoScreen;
